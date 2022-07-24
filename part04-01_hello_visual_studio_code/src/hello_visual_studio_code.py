badde8de06de63f91d650dd642f1ed23c11d370e
Ahmed <ahmedmo501296@gmail.com>
2022-07-24 18:48:03 +0100

    All tests passed

--- part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,19 @@
 # Write your solution here
+
+
+while True:
+   text = input("Editor:  ")
+
+   
+
+   if text.lower() == "notepad" or text.lower() == "word":
+      print("awful")
+
+
+   elif text.lower() == "visual studio code":
+        print("an excellent choice! ")
+        break
+   else:
+      print("not good")
+     
+   
