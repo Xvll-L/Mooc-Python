155f2523b2f20f16e02f001f97e85db9083e3e06
Ahmed <ahmedmo501296@gmail.com>
2022-07-24 18:59:04 +0100

    All tests passed

--- part04-02_line/src/line.py
+++ part04-02_line/src/line.py
@@ -1,4 +1,13 @@
 # Write your solution here
 # You can test your function by calling it within the following block
+def line(a,b):
+    if b == "":
+        print(a*"*")
+    else:
+        print(a * b[0])
+
 if __name__ == "__main__":
     line(5, "x")
+    line(7, "%")
+    line(10, "LOL")
+    line(3, "")
