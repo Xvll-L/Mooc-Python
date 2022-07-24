982928bf077cb07c4fad06a456ce6b570309ae04
Ahmed <ahmedmo501296@gmail.com>
2022-07-24 19:12:29 +0100

    All tests passed

--- part04-03_box_of_hashes/src/box_of_hashes.py
+++ part04-03_box_of_hashes/src/box_of_hashes.py
@@ -1,8 +1,19 @@
 # Copy here code of line function from previous exercise
+from re import I
+
+
+def line(a,b):
+    if b == "":
+        print(a*"*")
+    else:
+        print(a * b[0])
 
 def box_of_hashes(height):
     # You should call function line here with proper parameters
-    line(10, "#")
+    i = 0
+    while i < height:
+        i = i + 1
+        line(10, "#")
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
