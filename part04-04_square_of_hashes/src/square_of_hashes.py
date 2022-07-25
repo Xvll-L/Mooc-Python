6d61fb577591c534e264ed5a25d10cb92aff109c
Ahmed <ahmedmo501296@gmail.com>
2022-07-25 21:05:04 +0100

    All tests passed

--- part04-04_square_of_hashes/src/square_of_hashes.py
+++ part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,8 +1,16 @@
 # Copy here code of line function from previous exercise
-
+def line(a,b):
+    if b == "":
+        print(a*"*")
+    else:
+        print(a * b[0])
 def square_of_hashes(size):
     # You should call function line here with proper parameters
-    line(4, "#")
+    i = 0
+    while i < size:
+        i = i + 1
+        
+        line(size, "#")
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
