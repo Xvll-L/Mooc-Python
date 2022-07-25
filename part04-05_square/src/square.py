291683540fe0d9bc6c22460061c4ae33a672b70a
Ahmed <ahmedmo501296@gmail.com>
2022-07-25 21:11:27 +0100

    All tests passed

--- part04-05_square/src/square.py
+++ part04-05_square/src/square.py
@@ -1,8 +1,16 @@
 # Copy here code of line function from previous exercise
+def line(a,b):
+    if b == "":
+        print(a * "*")
+    else:
+        print(a * b[0])
 
-def nelio(size, character):
+def square(size, character):
     # You should call function line here with proper parameters
-    line(4, "x")
+    i = 0
+    while i < size:
+        i = i + 1
+        line(size, character)
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
