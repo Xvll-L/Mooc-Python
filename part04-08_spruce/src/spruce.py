46a5c0528663eb0b50fe48362ec5903534696ce9
Ahmed <ahmedmo501296@gmail.com>
2022-07-28 23:13:27 +0100

    All tests passed

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,23 @@
 # Write your solution here
+from re import I
+
+
+def spruce(a):
+    print("a spruce!")
+    i = 0
+    data = "*"
+    space = a - 1
+    print(f"{space * ' '}{data}")
+    loopval = a - 1
+    while i < loopval:
+        i = i + 1
+        space = space - 1
+        data = data + "**"
+        print(f"{space * ' '}{data}")
+    print(f"{(a - 1)* ' '}*")
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    spruce(5)
+    spruce(3)
+
+print(len("*********"))
