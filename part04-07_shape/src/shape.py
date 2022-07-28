80f82f41c0730d0bc7c27b9421fd0c816d452d15
Ahmed <ahmedmo501296@gmail.com>
2022-07-28 21:46:22 +0100

    All tests passed

--- part04-07_shape/src/shape.py
+++ part04-07_shape/src/shape.py
@@ -1,4 +1,23 @@
 # Copy here code of line function from previous exercise and use it in your solution
+def line(a,b):
+    if b == "":
+        print(a * "*")
+    else:
+        print(a * b[0])
+
+def shape(a,b,c,d):
+    i = 0
+    j = 0
+
+    while i < a:
+        i = i + 1
+        line(i, b)
+
+    while j < c:
+        j = j + 1
+        line(a, d)
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    shape(5, "x", 2, "o")
+    shape(5, "x", 3, "*")
