cbe384a3b66b16e655ab1e1d3bbd55bff0175659
Ahmed <ahmedmo501296@gmail.com>
2023-06-21 21:37:20 +0100

    Passed

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,14 @@
 # Write your solution here
+
+def same_chars(text, num1,num2):
+    if num1 >= len(text) or num2 >= len(text):
+        return False
+
+    if text[num1] == text[num2]:
+        return True
+    return False
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    print(same_chars("coder", 1, 12))
